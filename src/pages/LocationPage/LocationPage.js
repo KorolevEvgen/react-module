3c4872d4c699097954e3a339b566be105d7023e3
Maryna Plokhykh <korolev.evgen163@gmail.com>
2022-06-27 23:32:36 +0300

    react_router_dom

--- src/pages/LocationPage/LocationPage.js
+++ src/pages/LocationPage/LocationPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {locationService} from '../../services/location.service';
 import Location from '../../components/Location/Location';
+import './LocationPage.css'
 
 const LocationPage = () => {
     const [locations, setLocations] = useState([]);
